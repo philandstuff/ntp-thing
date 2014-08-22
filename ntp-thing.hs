811665ee052dfa6f4b08ee8ebc008e0983bd4f0b
Philip Potter <philip.g.potter@gmail.com>
2014-08-22 23:07:25 +0100

    send blank ntp packets
    based on the appendix to rfc958 [1]
    
    4 x 32-bit chunks of data followed by 4 x 64-bit timestamps == 48 byte
    packets.
    
    [1] http://tools.ietf.org/html/rfc958

--- ntp-thing.hs
+++ ntp-thing.hs
@@ -1,18 +1,45 @@
 import           Control.Monad            (liftM)
+import           Data.Binary.Builder (Builder, putWord32be, toLazyByteString)
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Lazy  as BL
+import           Data.Monoid      (mappend, mconcat)
+import           Data.Word        (Word32)
 import           Network.BSD
-import qualified Network.Socket     as NS
+import qualified Network.Socket            as NS
+import qualified Network.Socket.ByteString as NB
 import           System.Posix.Env (getEnvDefault)
 
 hostSocketAddress host port = do
   hostEntry <- getHostByName host
   return $ NS.SockAddrInet port (hostAddress hostEntry)
 
+sendMessage :: NS.SockAddr -> B.ByteString -> IO Int
 sendMessage socketAddress message = do
   sendSocket <- NS.socket NS.AF_INET NS.Datagram NS.defaultProtocol
-  NS.sendTo sendSocket message socketAddress
+  NB.sendTo sendSocket message socketAddress
+
+data Timestamp = Timestamp Word32 Word32
+
+putTimestamp :: Timestamp -> Builder
+putTimestamp (Timestamp i f) = putWord32be i `mappend` putWord32be f
+
+emptyPacket :: B.ByteString
+emptyPacket = B.concat $
+              BL.toChunks $
+              toLazyByteString $
+              mconcat [putWord32be 0,
+                       putWord32be 0,
+                       putWord32be 0,
+                       putWord32be 0,
+                       putTimestamp $ Timestamp 0 0,
+                       putTimestamp $ Timestamp 0 0,
+                       putTimestamp $ Timestamp 0 0,
+                       putTimestamp $ Timestamp 0 0]
+
+epoch = Timestamp 0 0
 
 main = do
   host <- getEnvDefault "HOST" "localhost"
-  port <- liftM read $ getEnvDefault "PORT" "123"
+  port <- liftM read $ getEnvDefault "PORT" "1025"
   addr <- hostSocketAddress host (toEnum port)
-  sendMessage addr "hello"
+  sendMessage addr $ emptyPacket
