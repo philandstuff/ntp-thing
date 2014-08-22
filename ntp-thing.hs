4a0b825962338205c63d74f5a4fb46fd08382328
Philip Potter <philip.g.potter@gmail.com>
2014-08-22 22:03:54 +0100

    default values if env vars not present

--- ntp-thing.hs
+++ ntp-thing.hs
@@ -1,7 +1,7 @@
 import           Control.Monad            (liftM)
 import           Network.BSD
 import qualified Network.Socket     as NS
-import           System.Environment       (getEnv)
+import           System.Posix.Env (getEnvDefault)
 
 hostSocketAddress host port = do
   hostEntry <- getHostByName host
@@ -11,9 +11,8 @@ sendMessage socketAddress message = do
   sendSocket <- NS.socket NS.AF_INET NS.Datagram NS.defaultProtocol
   NS.sendTo sendSocket message socketAddress
 
-
 main = do
-  host <- getEnv "HOST"
-  port <- liftM read $ getEnv "PORT"
+  host <- getEnvDefault "HOST" "localhost"
+  port <- liftM read $ getEnvDefault "PORT" "123"
   addr <- hostSocketAddress host (toEnum port)
   sendMessage addr "hello"
