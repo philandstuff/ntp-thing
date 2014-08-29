d0047d709cd32800e24be8febaf3392cab87ad65
Philip Potter <philip.g.potter@gmail.com>
2014-08-29 21:55:20 +0100

    Get real data back from ntp server
    Parse the packet we get back from the ntp server to discover the various
    timestamps in the message

--- ntp-thing.hs
+++ ntp-thing.hs
@@ -1,5 +1,7 @@
 import           Control.Exception        (bracket)
 import           Control.Monad            (liftM)
+import           Data.Binary     (Binary, decode, get, put)
+import           Data.Binary.Get (skip)
 import           Data.Binary.Put (Put, putWord8, putWord16be, putWord32be, runPut)
 import qualified Data.ByteString.Lazy  as BL
 import           Data.Word        (Word32)
@@ -20,10 +22,32 @@ withUdpSocket addr =
         return socket)
     NS.sClose
 
-data Timestamp = Timestamp Word32 Word32
+data Timestamp = Timestamp Word32 Word32 deriving Show
 
 putTimestamp :: Timestamp -> Put
 putTimestamp (Timestamp i f) = putWord32be i >> putWord32be f
+
+getTimestamp = do
+  i <- get
+  f <- get
+  return $ Timestamp i f
+
+data Ntp2Packet =
+  Ntp2SyncPacket { reference :: Timestamp,
+                   originate :: Timestamp,
+                   receive   :: Timestamp,
+                   transmit  :: Timestamp}
+  deriving Show
+
+instance Binary Ntp2Packet where
+  put = undefined
+  get = do
+    skip 16
+    ref  <- getTimestamp
+    orig <- getTimestamp
+    rx   <- getTimestamp
+    tx   <- getTimestamp
+    return $ Ntp2SyncPacket ref orig rx tx
 
 emptyPacket :: BL.ByteString
 emptyPacket = runPut $
@@ -51,3 +75,4 @@ main = do
                     NB.recv socket 1024)
   putStrLn $ show $ BL.unpack recvPacket
   putStrLn $ show recvPacket
+  putStrLn $ show $ (decode recvPacket :: Ntp2Packet)
