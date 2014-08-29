fc183622170072cbcf6a921006cc8368a271a675
Philip Potter <philip.g.potter@gmail.com>
2014-08-29 17:09:02 +0100

    use higher-level Data.Binary.Put not Builder

--- ntp-thing.hs
+++ ntp-thing.hs
@@ -1,8 +1,7 @@
 import           Control.Monad            (liftM)
-import           Data.Binary.Builder (Builder, putWord32be, toLazyByteString)
+import           Data.Binary.Put (Put, putWord32be, runPut)
 import qualified Data.ByteString       as B
 import qualified Data.ByteString.Lazy  as BL
-import           Data.Monoid      (mappend, mconcat)
 import           Data.Word        (Word32)
 import           Network.BSD
 import qualified Network.Socket            as NS
@@ -20,21 +19,21 @@ sendMessage socketAddress message = do
 
 data Timestamp = Timestamp Word32 Word32
 
-putTimestamp :: Timestamp -> Builder
-putTimestamp (Timestamp i f) = putWord32be i `mappend` putWord32be f
+putTimestamp :: Timestamp -> Put
+putTimestamp (Timestamp i f) = putWord32be i >> putWord32be f
 
 emptyPacket :: B.ByteString
 emptyPacket = B.concat $
               BL.toChunks $
-              toLazyByteString $
-              mconcat [putWord32be 0,
-                       putWord32be 0,
-                       putWord32be 0,
-                       putWord32be 0,
-                       putTimestamp $ Timestamp 0 0,
-                       putTimestamp $ Timestamp 0 0,
-                       putTimestamp $ Timestamp 0 0,
-                       putTimestamp $ Timestamp 0 0]
+              runPut $
+              do putWord32be 0
+                 putWord32be 0
+                 putWord32be 0
+                 putWord32be 0
+                 putTimestamp $ Timestamp 0 0
+                 putTimestamp $ Timestamp 0 0
+                 putTimestamp $ Timestamp 0 0
+                 putTimestamp $ Timestamp 0 0
 
 epoch = Timestamp 0 0
 
