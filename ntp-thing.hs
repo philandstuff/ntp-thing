e509331bf9aefda9083c92d913495e09ca8e885e
Philip Potter <philip.g.potter@gmail.com>
2014-08-30 08:17:55 +0100

    parse leap indicator, version, mode, stratum

--- ntp-thing.hs
+++ ntp-thing.hs
@@ -1,10 +1,11 @@
 import           Control.Exception        (bracket)
 import           Control.Monad            (liftM, liftM2, liftM4)
 import           Data.Binary     (Binary, decode, get, put)
-import           Data.Binary.Get (Get, skip)
+import           Data.Binary.Get (Get, getWord8, skip)
 import           Data.Binary.Put (Put, putWord8, putWord16be, putWord32be, runPut)
+import           Data.Bits
 import qualified Data.ByteString.Lazy  as BL
-import           Data.Word        (Word32)
+import           Data.Word        (Word8, Word32)
 import           Network.BSD
 import qualified Network.Socket            as NS
 import qualified Network.Socket.ByteString.Lazy as NB
@@ -31,8 +32,26 @@ getTimestamp :: Get Timestamp
 getTimestamp =
   liftM2 Timestamp get get
 
+data LeapIndicator = None | AddSecondToday | DropSecondToday | Unknown
+                   deriving (Show, Enum)
+
+data Mode = Reserved | SymmActive | SymmPassive | Client | Server | Broadcast | NtpControl | PrivateUse
+          deriving (Show, Enum)
+
+getLeapVersionMode :: Get (LeapIndicator, Word8, Mode)
+getLeapVersionMode = do
+  word <- getWord8
+  let li = toEnum $ fromIntegral $ word `shiftR` 6
+  let version = (word `shiftR` 3) .&. 0x7
+  let mode = toEnum $ fromIntegral $ word .&. 0x7
+  return (li,version,mode)
+
 data Ntp2Packet =
-  Ntp2SyncPacket { reference :: Timestamp,
+  Ntp2SyncPacket { li        :: LeapIndicator,
+                   version   :: Word8,
+                   mode      :: Mode,
+                   stratum   :: Word8,
+                   reference :: Timestamp,
                    originate :: Timestamp,
                    receive   :: Timestamp,
                    transmit  :: Timestamp}
@@ -41,8 +60,11 @@ data Ntp2Packet =
 instance Binary Ntp2Packet where
   put = undefined
   get = do
-    skip 16
-    liftM4 Ntp2SyncPacket getTimestamp getTimestamp getTimestamp getTimestamp
+    (li,version,mode) <- getLeapVersionMode
+    let packetSoFar = Ntp2SyncPacket li version mode
+    packetSoFar <- liftM packetSoFar getWord8
+    skip 14
+    liftM4 packetSoFar getTimestamp getTimestamp getTimestamp getTimestamp
 
 emptyPacket :: BL.ByteString
 emptyPacket = runPut $
